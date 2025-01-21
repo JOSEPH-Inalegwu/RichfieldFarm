f877a34b9bb6a21e256cb62c4cc5dddbeda9772d
INALEGWU-Joseph <josephjonahinalegwu@gmail.com>
2025-01-21 06:05:24 +0100

    Add new rating images and enhanced the messages

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react"
 import proilePic01 from '../../assets/profilepic01.jpg'
 import proilePic02 from '../../assets/profilepic02.jpg'
 
+// Ratings
+import rating40 from '../../assets/ratings/rating-40.png'
+import rating45 from '../../assets/ratings/rating-45.png'
 
 const Testimonial = () => {
     const [testimonials, setTestimonials] = useState([]);  
@@ -11,25 +14,28 @@ const Testimonial = () => {
       // Simulate fetching data
       const fetchedTestimonials = [
         {
-          name: 'John Doe',
-          jobTitle: 'CEO, Company',
+          name: 'David Turner',
+          jobTitle: 'Food Blogger at Everyday Delights:',
           profile: proilePic02,
           message:
-            'The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive, and the delivery was impressively prompt.'
+            'I love shopping here for my weekly groceries! The farm-fresh produce is vibrant and flavorful, making my meals healthier and tastier. This site has become my go-to for quality ingredients',
+          rating: rating40
         },
         {
-          name: 'Jane Smith',
-          jobTitle: 'CTO, Another Company',
+          name: 'Maria Gonzalez',
+          jobTitle: 'Executive Chef at Green Plate Bistro',
           profile: proilePic01,
           message:
-            'I loved the professionalism and dedication to quality. The team really exceeded expectations!'
+            '"The freshness of the ingredients has transformed my dishes. Every vegetable and fish is top-quality. I highly recommend their products to any chef seeking excellence.',
+          rating: rating45
         },
         {
-          name: 'Alex Johnson',
-          jobTitle: 'Manager, Business Inc.',
+          name: 'Sophia Carter',
+          jobTitle: 'Nutritionist, Wellness Center',
           profile: 'https://readymadeui.com/team-3.webp',
           message:
-            'Absolutely fantastic service! It was quick, seamless, and stress-free. Highly recommended!'
+            'Thanks to RichField\'s reliable products, I can confidently recommend healthy and sustainable options to my clients. Their commitment to quality and innovation sets them apart in the industry',
+          rating: rating45
         }
       ];
   
@@ -38,10 +44,10 @@ const Testimonial = () => {
 
   return (
     <>
-      <div className="p-4 my-6 font-[sans-serif] max-w-6xl max-lg:max-w-3xl max-md:max-w-xl mx-auto">
+      <div className="p-4 my-10 font-[sans-serif] max-w-6xl max-lg:max-w-3xl max-md:max-w-xl mx-auto">
       <div className="max-w-2xl">
         <h2 className="text-gray-800 text-2xl font-bold md:text-3xl">What our happy client say</h2>
-        <p className="text-sm text-gray-500 mt-4 leading-relaxed">Veniam proident aute magna anim excepteur et ex consectetur velit ullamco veniam minim aute sit. Elit occaecat officia et laboris Lorem minim. Officia do aliqua adipisicing ullamco in.</p>
+        <p className="text-sm text-gray-500 mt-4 leading-relaxed">Our clients rave about the quality and freshness of our products. They appreciate the dedication we put into sourcing the best ingredients. With every order, we strive to exceed their expectations and provide an exceptional experience.</p>
       </div>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-md:justify-center mt-12">
@@ -49,7 +55,7 @@ const Testimonial = () => {
         <>
           <div key={index} className="border border-gray-300 lg:p-6 p-4 rounded-xl bg-white relative">
           <div className="flex items-center">
-            <img src={testimonial.profile} className="w-14 h-14 rounded-full border border-purple-600" />
+            <img src={testimonial.profile} className="w-14 h-14 rounded-full border border-teal-600" />
             <div className="ml-4">
               <h6 className="text-gray-800 text-sm font-semibold"> {testimonial.name} </h6>
               <p className="text-xs text-gray-500 mt-1">
@@ -65,31 +71,7 @@ const Testimonial = () => {
           </div>
 
           <div className="flex space-x-1 mt-4">
-            <svg className="w-4 h-4 fill-purple-600" viewBox="0 0 14 13" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-            </svg>
-            <svg className="w-4 h-4 fill-purple-600" viewBox="0 0 14 13" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-            </svg>
-            <svg className="w-4 h-4 fill-purple-600" viewBox="0 0 14 13" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-            </svg>
-            <svg className="w-4 h-4 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-            </svg>
-            <svg className="w-4 h-4 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-            </svg>
+            <img src={testimonial.rating} className="w-22 h-5" />
           </div>
           </div>
         </>
