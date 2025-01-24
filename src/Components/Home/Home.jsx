5463cb3e77c32cdde724162d6b47a68b275a92dd
INALEGWU-Joseph <josephjonahinalegwu@gmail.com>
2025-01-24 11:26:15 +0100

    Add FruitCard component with scrollable fruit items and navigation arrows

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import Hero from "../Hero/Hero"
 import Testimonial from "../Testimonial/Testimonial"
 import FirstContent from "../FirstContent/FirstContent"
+import FruitCard from "../FruitCard/FruitCard"
+
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FruitCard />
       <FirstContent />
       <Testimonial />
     </>
