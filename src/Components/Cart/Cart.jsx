6a9df329a8324bb4892aaa08f90a4de8466d331d
JOSEPH-Inalegwu <josephjonahinalegwu@gmail.com>
2025-03-25 02:26:50 +0100

    Refactor code formatting and update SVG files for consistency

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -35,7 +35,7 @@ const Cart = () => {
             No item in the cart
            </p>
   
-          <Link to='/'
+          <Link to='/product'
             className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
           >
             Add Product
